0c40189029f9a5327591c9a5375c32c5b87fb4bf
zhiqiang.wang <510342804@qq.com>
2022-10-15 21:35:02 +0800

    delelte one file

--- 错误运动点数/functions/breathingDataScore_slope.m
+++
@@ -1,9 +0,0 @@
-function [ score ] = breathingDataScore_slope( dataIn )
-envelope_points = 300;
-[up, down] = envelope(dataIn,envelope_points,'peak');  % 求包络线的方式1
-n = 0:1:size(dataIn,2)-1;
-a1 = polyfit(n,down,1); % 1阶多项式最小二乘拟合
-y1 = polyval(a1,n);
-score = abs( 1 / ( (n(1,2) - n(1,1)) / (y1(1,2) - y1(1,1)) ) );
-end
-
